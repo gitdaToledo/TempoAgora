bbf5f60e9fe358938d960e5f2482bcbe1782b1ff
Maria Paula <maripaulaa6@gmail.com>
2025-09-21 20:19:20 -0300

    Adicionar arquivos de projeto.

---
+++ TempoAgora/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TempoAgora
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
